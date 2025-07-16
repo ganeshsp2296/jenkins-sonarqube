d031172945aab76cd72f8245f7783e161f4ce0fa
Ubuntu <ubuntu@ip-172-31-43-23.ap-south-1.compute.internal>
2025-07-16 06:54:35 +0000

    Added duplicated method to trigger SonarQube duplication

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,4 +2,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins + SonarQube!");
     }
+
+    // To trigger SonarQube duplication
+    public static void duplicatedMain() {
+        System.out.println("Hello from Jenkins + SonarQube!");
+    }
 }
+
